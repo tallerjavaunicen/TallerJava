cdb25121e2caba5152501d935cca6aa3312a1d0a
Juan Manuel Rodriguez <juanmanuel.rodriguez@isistan.unicen.edu.ar>
2021-04-19 16:03:17 -0300

    Add enter behaviour to the text input chat GUI

--- ChatGUI/src/edu/isistan/chat/gui/MainPanel.java
+++ ChatGUI/src/edu/isistan/chat/gui/MainPanel.java
@@ -4,6 +4,8 @@ import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.LinkedList;
@@ -100,9 +102,34 @@ public class MainPanel extends JPanel {
                 }
             }
         });
+        
+        sentMsg.addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {
+				
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (KeyEvent.VK_ENTER == e.getKeyCode()) {
+					if(!sentMsg.getText().trim().equals("")) {
+	                    MainWindows.getIChat().sendMsg(sentMsg.getText());
+	                    maintxt.setText(maintxt.getText()+"...: "+sentMsg.getText()+"\n");
+	                    sentMsg.setText("");
+	                }
+				}
+			}
+		});
 
     }
-    
+   
     public void remevoUser(String user) {
         listUsers.remove(user);
         users.setListData(listUsers.toArray(new String[listUsers.size()]));
--- ChatGUI/src/edu/isistan/chat/gui/UserPanel.java
+++ ChatGUI/src/edu/isistan/chat/gui/UserPanel.java
@@ -4,6 +4,8 @@ import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -55,6 +57,31 @@ public class UserPanel extends JPanel {
                 }
             }
         });
+        
+        sentMsg.addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {
+				
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (KeyEvent.VK_ENTER == e.getKeyCode()) {
+					if(!sentMsg.getText().trim().equals("")) {
+	                    MainWindows.getIChat().sendMsg(UserPanel.this.user, sentMsg.getText());
+	                    maintxt.setText(maintxt.getText()+"...: "+sentMsg.getText()+"\n");
+	                    sentMsg.setText("");
+	                }
+				}
+			}
+		});
 
     }
     public void addNewMessage(String text) {
